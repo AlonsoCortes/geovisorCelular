7d07d7e2ef8cfb2102791eb69d2110340f0db1e5
alonso.cortes <alo.acs@gmail.com>
2025-10-29 16:02:55 -0600

    acercamiento segundo filtro

--- src/js/01_cargarCapas.js
+++ src/js/01_cargarCapas.js
@@ -2,6 +2,22 @@ export function cargarCapas(map) {
   const estilos = getComputedStyle(document.documentElement);
 
   map.on('load', () => {
+    // === Capa de municipios ===
+    map.addSource('municipios_source', {
+      type: 'geojson',
+      data: './src/assets/data/entidades_municipal.geojson'
+    });
+
+    map.addLayer({
+      id: 'municipios_layer',
+      type: 'line',
+      source: 'municipios_source',
+      paint: {
+        'line-color': '#000000ff',
+        'line-width': 0.1,
+        'line-opacity': 0.1,
+      }
+    });
     // === Capa de estados ===
     map.addSource('estados_source', {
       type: 'geojson',
@@ -10,14 +26,15 @@ export function cargarCapas(map) {
 
     map.addLayer({
       id: 'estados_layer',
-      type: 'fill',
+      type: 'line',
       source: 'estados_source',
       paint: {
-        'fill-color':'rgba(0, 0, 0, 0)' ,
-        'fill-opacity': 1,
-        'fill-outline-color': '#000000'
-        }
+        'line-color': '#000000ff',
+        'line-width': 0.5,
+        'line-opacity': 1,
+      }
     });
+    
     // === Capa de proyectos ===
     map.addSource('infra_source', {
       type: 'geojson',
--- src/js/02_filtrosAnidados.js
+++ src/js/02_filtrosAnidados.js
@@ -1,4 +1,4 @@
-
+import { acercamientoEntidad } from "./03_acercamientoFiltro.js";
 
 export function filtrosAnidados(map) {
   const filtroPrimario = document.getElementById('filtro1');
@@ -38,43 +38,13 @@ export function filtrosAnidados(map) {
       map.setFilter('infra_layer', null);
       return;
     }
-
-    // PRUEBA
-    if (estadoSeleccionado !== "all") {
-          fetch("./src/assets/data/entidades_estatales.geojson")
-            .then((r) => r.json())
-            .then((entidades) => {
-              const entidad = entidades.features.find(
-                (f) => f.properties.NOMGEO === estadoSeleccionado
-              );
-
-              if (entidad) {
-                const bounds = new maplibregl.LngLatBounds();
-
-                // Soporte para polígonos y multipolígonos
-                if (entidad.geometry.type === "Polygon") {
-                  entidad.geometry.coordinates[0].forEach((c) => bounds.extend(c));
-                } else if (entidad.geometry.type === "MultiPolygon") {
-                  entidad.geometry.coordinates
-                    .flat(2)
-                    .forEach((c) => bounds.extend(c));
-                }
-
-                // Aplicar zoom al extent
-                map.fitBounds(bounds, {
-                  padding: 60,
-                  duration: 1000,
-                });
-              }
-            });
-        }
-
-
-
-
-
-
-    // FIN PRUEBA
+    // Acercamiento a entidad seleccionada
+    acercamientoEntidad(
+      map,
+      './src/assets/data/entidades_estatales.geojson',
+      'NOMGEO',
+      estadoSeleccionado
+    )
 
     // Filtrar municipios que correspondan al estado seleccionado
     const municipios = [
@@ -112,6 +82,20 @@ export function filtrosAnidados(map) {
       // Mostrar solo el municipio seleccionado
       map.setFilter('infra_layer', ['==', ['get', 'ent_mun'], municipioSeleccionado]);
     }
+     // Acercamiento a municipios
+    // Acercamiento a entidad seleccionada
+    acercamientoEntidad(
+      map,
+      './src/assets/data/entidades_municipal.geojson',
+      'nom_mun',
+      municipioSeleccionado
+    )
   });
+
+ 
+
+
+
+
 }
 
